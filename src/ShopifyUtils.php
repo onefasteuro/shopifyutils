fcd252b495683d166fe9dd14caf4e9a176e31969
Oliver <onefasteuro@gmail.com>
2019-10-11 18:55:54 -0400

    Edits

--- src/ShopifyUtils.php
+++ src/ShopifyUtils.php
@@ -4,5 +4,30 @@ namespace onefasteuro\ShopifyUtils;
 
 class ShopifyUtils
 {
+    
+    const URL_AUTHORIZE = 'https://%s.myshopify.com/admin/oauth/authorize?client_id=%s&scope=%s&state=%s&redirect_uri=%s';
+    const URL_FOR_TOKEN = 'https://%s/admin/oauth/access_token';
 
+
+    /**
+     * Parses a GID and returns an id
+     * @param $gid
+     * @return int
+     */
+    public static function gidParse($gid)
+    {
+        return intval(preg_replace('/[^0-9]/', '', $gid));
+    }
+
+    /**
+     * Restore an id into a GID
+     * @param $id
+     * @param $namespace
+     * @return string
+     */
+    public static function gidRestore($id, $namespace)
+    {
+        $id = (int) $id;
+        return sprintf('gid://shopify/%s/%d', $namespace, $id);
+    }
 }
