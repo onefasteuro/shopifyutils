9a7f2fbeba03ccd5953a0fe078c0807c2ed27dc8
onefasteuro <onefasteuro@gmail.com>
2019-10-20 10:41:16 -0400

    Update ShopifyUtils.php
    Moved domain protocol to fqdn function

--- src/ShopifyUtils.php
+++ src/ShopifyUtils.php
@@ -41,7 +41,7 @@ class ShopifyUtils
         return sprintf('gid://shopify/%s/%d', $namespace, $id);
     }
 
-    public static function formatDomain($domain, $protocol = false)
+    public static function formatDomain($domain)
     {
         $domain = trim($domain);
         $domain = rtrim($domain, '/');
@@ -52,56 +52,13 @@ class ShopifyUtils
             $domain = sprintf('%s.myshopify.com', $domain);
         }
 
-        return ($protocol === false) ? $domain : sprintf('https://%s', $domain);
+        return $domain;
     }
 	
-	/**
-	 * Extract headers from curl response
-	 * @param $response
-	 * @return array
-	 */
-	public static function parseHeaders($response)
+	public static function formatFqdn($domain)
 	{
-		$split = explode("\r\n\r\n", $response);
-		$headers_section = trim( strstr($split[0], "\r\n") );
-		unset($split);
-		
-		$headers = [];
-		
-		$headers_array = explode("\r\n", $headers_section);
-		foreach($headers_array as $header)
-		{
-			$h = explode(': ', $header);
-			$headers[$h[0]] = $h[1];
-		}
-		
-		return $headers;
+		$domain = static::formatDomain($domain);
+		return sprintf('https://%s', $domain);
 	}
-	
-	/**
-	 * Extract the status code ouf of a curl response
-	 * @param $response
-	 * @return int
-	 */
-	public static function parseStatusCode($response)
-	{
-		$code = strstr($response,"\r\n",true);
-		preg_match('/\d{3}/', $code, $matches);
-		
-		
-		return (count($matches) > 0) ? (int) $matches[0] : 0;
-	}
-	
-	/**
-	 * Extract the body from a curl response
-	 * @param $response
-	 * @return mixed
-	 */
-	public static function parseBody($response)
-	{
-		$split = explode("\r\n\r\n", $response);
-		return $split[1];
-	}
-
 
 }
